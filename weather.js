c7c883737a177ea2da91557b47ff0cfaebd66b9e
vic6 <vicmatthews116@gmail.com>
2017-02-16 22:22:56 -0800

    Remove comments, adjust layout

--- weather.js
+++ weather.js
@@ -1,26 +1,6 @@
 $(document).ready(function() {
 });
 
-
-// $(document).ready(function() {
-//   $.simpleWeather({
-//     location: 'Austin, TX',
-//     woeid: '',
-//     unit: 'f',
-//     success: function(weather) {
-//       html = '<h2><i class="icon-'+weather.code+'"></i> '+weather.temp+'&deg;'+weather.units.temp+'</h2>';
-//       html += '<ul><li>'+weather.city+', '+weather.region+'</li>';
-//       html += '<li class="currently">'+weather.currently+'</li>';
-//       html += '<li>'+weather.wind.direction+' '+weather.wind.speed+' '+weather.units.speed+'</li></ul>';
-//
-//       $("#weather").html(html);
-//     },
-//     error: function(error) {
-//       $("#weather").html('<p>'+error+'</p>');
-//     }
-//   });
-// });
-
 var getWeather = function() {
   var location = $('#srch-term').val();
   var expression = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
@@ -34,18 +14,13 @@ var getWeather = function() {
       var description = data.query.results.channel.item.condition.text;
       var image = data.query.results.channel.item.description;
       var title = data.query.results.channel.title;
-      var high
-      var low
-
       // debugger
-
       html = '<h2>'+title+'</h2>';
+      html += '<ul><li>'+"Today - " + description+'</li>';
       html += '<h2>'+temp+'&deg;'+'</h2>';
 
-      html += '<ul><li>'+description+'</li>';
 
 
-      // html += '<li class="currently">'+forecast+'</li>';
       $('.media-heading').text(location);
       $('#media').attr('src', image.match(expression)[0]);
       for(day in forecast) {
